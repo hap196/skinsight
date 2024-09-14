7877187055dc235b01cee27dccaf1a322b9d6e42
Hailey <haileyrpan@gmail.com>
2024-09-14 16:10:27 -0400

    redirect to quiz

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   const navigate = useNavigate();
 
   const handleGetStarted = () => {
-    navigate('/predict');
+    navigate('/quiz');
   };
   
   const handleLogin = () => {
