efb668a1cd2e27d6757c799148bb1b43d293e571
hannahTao <56075416+hannahTao@users.noreply.github.com>
2024-09-15 03:03:01 -0400

    scrolling on home

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -7,6 +7,13 @@ const Home = () => {
     window.location.href = "http://localhost:5001/login";
   };
 
+  const scrollToBottom = () => {
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      behavior: 'smooth' // Smooth scrolling
+    });
+  };
+
   return (
     <div className="home">
       <section className="section1">
@@ -14,17 +21,17 @@ const Home = () => {
           sk<span className="highlight">insight</span>
         </h1>
         <p>ACHIEVING HEALTHY, BEAUTIFUL SKIN</p>
-        <button className="get-started-button" onClick={handleLogin}>GET STARTED</button>
+        <button className="get-started-button" onClick={scrollToBottom}>GET STARTED</button>
       </section>
       <section className="section2">
-        <h2>Skincare Recommendations</h2>
+        <h2>skincare recommendations</h2>
         <h4>Powered with AI</h4>
         <p>We use an AI model trained on dermatology datasets to identify your skin conditions in real time.
           Complete a quiz and upload a photo of your skin to receive personalized skincare recommendations.
         </p>
       </section>
       <section className="section3">
-        <h2>No brands, just ingredients.</h2>
+        <h2>no brands, just ingredients.</h2>
         <p>
           Just 7 companies control almost all the beauty products you buy. Given the concentration of the fashion market, it becomes hard to see past the brands. At Skinsight, we will help you look for what matters: the ingredients.
         </p>
